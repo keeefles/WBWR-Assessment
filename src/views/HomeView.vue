6408e5d8d50a100590bb026a2b6660c8f63aa17f
keeefles <aakeefahj@gmail.com>
2024-05-06 17:01:55 +0200

    got somewhere

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -6,33 +6,46 @@
       </iframe>
 
       <div class="heading1">
-        <h1 class="idk">Check out our latest gear</h1>
+        <h1 class="idk">CHECK OUT OUR LATEST GEAR</h1>
       </div>
       <div class="h1-text">Advanced tech, superior comfort and all performance is what our latest UA gear is all about. Built to go further, push hard and break your boundaries.</div>
-      <div class="button">
-        <button>Shop UA New Arrivals</button>
+      <div class="bottun">
+        <button class="button">Shop UA New Arrivals</button>
       </div>
     </div>
   </section><br>
 <!-- end of landing -->
 <!-- start of categories -->
   <section>
-    <h4>Our best gear</h4>
+    <h2 class="gear-bear">Our best gear</h2>
       <p>Unlock your potential with the best UA Gear.</p>
 <!-- images inserted -->
-    <ul>
-      <li><img src="" alt="Train">Shop Now</li>
-      <li><img src="" alt="Run">Shop Now</li>
-      <li><img src="" alt="Golf">Shop Now</li>
-      <li><img src="" alt="Accessories">Shop Now</li>
-    </ul><br>
+  <div class="gear-images">
+    <div class="gb-img">
+      <img src="https://iili.io/JrRvbRe.md.jpg" class="gb-img" alt="Train">Shop Now
+    </div>
+    <div class="gb-img">
+      <img src="https://iili.io/JrR8AgI.md.jpg" class="gb-img" alt="Run">Shop Now
+    </div>
+    <div class="gb-img">
+      <img src="https://iili.io/JrR8BOg.md.jpg" class="gb-img" alt="Golf">Shop Now
+    </div>
+    <div class="gb-img">
+      <img src="https://iili.io/JrR80Xf.md.jpg" class="gb-img" alt="Accessories">Shop Now
+    </div>
+  </div>
+  <br>
+</section>
 <!-- end of categories -->
 <!-- start of Project Rock -->
-    <div class="project rock">
-      <img src="" alt="Project Rock img">
+  <section>
+    <div class="project-rockInfo">
+      <div class="project-rock">
+        <img src="https://iili.io/JrRgM1R.md.jpg" width="1000" alt="Project Rock img">
+      </div>
       <p>Project Rock</p><br/>
       <h3 class="bold-text">Every side of strong</h3><br>
-        <p class="pr-text">The Underground collection is inspired by the idea of a Project Rock fight club. A place wher we push each other harder to make each toher stronger. A drop dedicated to you and your crew. The neon colourways are a reflection of the energy we bring to every rep, every set and every challenge.</p><br>
+        <p class="pr-text">The Underground collection is inspired by the idea of a Project Rock fight club. A place where we push each other harder to make each toher stronger. A drop dedicated to you and your crew. The neon colourways are a reflection of the energy we bring to every rep, every set and every challenge.</p><br>
       <button class="btn">Shop Project Rock</button>
     </div>
   </section>
@@ -42,10 +55,10 @@
     <div class="promo">
       <h3>Discover out lastest promotions</h3>
       <p>Get the best deals on our best gear.</p>
-        <img src="" alt="buy 2 for R700">
-        <img src="" alt="buy 2 for R800">
-        <img src="" alt="buy 2 for R1000">
-        <img src="" alt="buy 2 for R1200">
+        <img src="https://iili.io/JrR8LhJ.jpg" alt="buy 2 for R700">
+        <img src="https://iili.io/JrRSETg.jpg" alt="buy 2 for R800">
+        <img src="https://iili.io/JrRS9kX.jpg" alt="buy 2 for R1000">
+        <img src="https://iili.io/JrRSfEl.jpg" alt="buy 2 for R1200">
     </div>
   </section>
 <!-- end of promo -->
@@ -56,10 +69,10 @@
 <!-- insert video here somehow -->
       </div>
       <div class="images">
-        <img src="" alt="dwayne wearing a project rock jacket">
-        <img src="" alt="random light skinned guy">
-        <img src="" alt="girl running for the gram">
-        <img src="" alt="the rock lifting his body weight">
+        <img src="https://iili.io/JrRUSHP.md.jpg" alt="dwayne wearing a project rock jacket">
+        <img src="https://iili.io/JrRgfJS.md.jpg" alt="random light skinned guy">
+        <img src="https://iili.io/JrRgxWb.md.jpg" alt="girl running for the gram">
+        <img src="https://iili.io/JrRgAgV.md.jpg" alt="the rock lifting his body weight">
       </div>
     </div>
   </section>
