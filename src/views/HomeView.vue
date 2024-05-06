2211374e2cdb0933967ca3acb68ea2ea522394ca
keeefles <aakeefahj@gmail.com>
2024-05-06 20:51:28 +0200

    done for the night

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -2,13 +2,14 @@
 <!-- start of landing -->
   <section class="home">
     <div class="video">
-      <iframe src="https://drive.google.com/file/d/1Wb1RCZBvnkF5waj6aQ7pmnyNkDXJWHFe/preview" class="video1" allow="autoplay">
+      <iframe src="https://drive.google.com/file/d/1Wb1RCZBvnkF5waj6aQ7pmnyNkDXJWHFe/preview" class="video1" height="500" allow="autoplay">
       </iframe>
+
 
       <div class="heading1">
         <h1 class="idk">CHECK OUT OUR LATEST GEAR</h1>
       </div>
-      <div class="h1-text">Advanced tech, superior comfort and all performance is what our latest UA gear is all about. Built to go further, push hard and break your boundaries.</div>
+      <div class="h1-text">Advanced tech, superior comfort and all performance is what our latest UA gear is all about. Built to go further, push hard and break your boundaries.</div><br>
       <div class="bottun">
         <button class="button">Shop UA New Arrivals</button>
       </div>
@@ -41,40 +42,50 @@
   <section>
     <div class="project-rockInfo">
       <div class="project-rock">
-        <img src="https://iili.io/JrRgM1R.md.jpg" width="1000" alt="Project Rock img">
+        <img src="https://iili.io/JrRgM1R.md.jpg" height="500" alt="Project Rock img">
       </div>
-      <p>Project Rock</p><br/>
-      <h3 class="bold-text">Every side of strong</h3><br>
+      <div>
+        <p>Project Rock</p><br/>
+        <h2 class="bold-text">Every side of strong</h2><br>
         <p class="pr-text">The Underground collection is inspired by the idea of a Project Rock fight club. A place where we push each other harder to make each toher stronger. A drop dedicated to you and your crew. The neon colourways are a reflection of the energy we bring to every rep, every set and every challenge.</p><br>
-      <button class="btn">Shop Project Rock</button>
+        <button class="btn">Shop Project Rock</button>
+      </div>
     </div>
   </section>
 <!-- end of project rock -->
 <!-- start of promotions -->
   <section>
     <div class="promo">
-      <h3>Discover out lastest promotions</h3>
+      <h2 class="gear-bear">Discover out lastest promotions</h2>
       <p>Get the best deals on our best gear.</p>
-        <img src="https://iili.io/JrR8LhJ.jpg" alt="buy 2 for R700">
-        <img src="https://iili.io/JrRSETg.jpg" alt="buy 2 for R800">
-        <img src="https://iili.io/JrRS9kX.jpg" alt="buy 2 for R1000">
-        <img src="https://iili.io/JrRSfEl.jpg" alt="buy 2 for R1200">
+      <div class="promo-img">
+        <img src="https://iili.io/JrR8LhJ.jpg" id="promo" alt="buy 2 for R700">
+        <img src="https://iili.io/JrRSETg.jpg" id="promo" alt="buy 2 for R800">
+        <img src="https://iili.io/JrRS9kX.jpg" id="promo" alt="buy 2 for R1000">
+        <img src="https://iili.io/JrRSfEl.jpg" id="promo" alt="buy 2 for R1200">
+      </div>
     </div>
   </section>
 <!-- end of promo -->
 <!-- start of insta -->
   <section>
     <div class="insta">
-      <div class="video">
+      <h2 class="gear-bear">What's happening on Instagram</h2>
+      <p>Don't miss out on the latest news and updates from Under Armour.</p>
+      <div class="row">
+        <div class="video">
 <!-- insert video here somehow -->
       </div>
       <div class="images">
         <img src="https://iili.io/JrRUSHP.md.jpg" alt="dwayne wearing a project rock jacket">
         <img src="https://iili.io/JrRgfJS.md.jpg" alt="random light skinned guy">
+      <div class="images">
         <img src="https://iili.io/JrRgxWb.md.jpg" alt="girl running for the gram">
         <img src="https://iili.io/JrRgAgV.md.jpg" alt="the rock lifting his body weight">
       </div>
     </div>
+    </div>
+    </div>  
   </section>
 </template>
 
