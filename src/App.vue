d42986e3ad86539291afce0d908d1879785f037e
keeefles <aakeefahj@gmail.com>
2024-05-06 11:42:56 +0200

    error fixed

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,19 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
-  <router-view/>
+  <HeaderView/>
+  <FooterView/>
 </template>
+
+<script>
+import FooterView from '@/components/FooterView.vue';
+import HeaderView from '@/components/HeaderView.vue';
+
+export default {
+  components: {
+    HeaderView,
+    FooterView,
+  },
+}
+</script>
 
 <style>
 #app {
